9624ad3772687e68a1e32d976bc19193e073baa3
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-09-12 01:43:44 +0300

    Update NewsCard.js

--- Newsletter/src/Components/NewsCard/NewsCard.js
+++ Newsletter/src/Components/NewsCard/NewsCard.js
@@ -5,7 +5,7 @@ import styles from './NewsCard.style';
 const NewsCard = ({news}) => {
     return (
     <View style={styles.container}>
-        <Image style={styles.Image} source={{url: news.imageUrl}}/>
+        <Image style={styles.Image} source={{uri: news.imageUrl}}/>
         <Text style={styles.title}>{news.title}</Text>
         <Text style={styles.description}>{news.description}</Text>
     </View>
